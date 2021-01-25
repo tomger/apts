40c1c9c640e04d858a2e2d081298fe6f48ef6253
tom <tom.germeau@gmail.com>
2021-01-24 19:14:34 -0500

    script

--- pull.sh
+++ pull.sh
@@ -1,3 +1,4 @@
-curl https://brodsky.com/rentals/downtown-brooklyn/city-tower > city-tower.html
+curl https://brodsky.com/rentals/downtown-brooklyn/city-tower | hxnormalize -x | hxselect -i "div.apt-grid" > city-tower.html
+git add city-tower.html
 git commit --allow-empty-message -m ""
-git push origin
+git push origin master
